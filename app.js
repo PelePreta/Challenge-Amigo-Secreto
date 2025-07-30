b65bf4f4d3a6a68cd2e75b65a2dab459c633dd0f
Stéfany de Oliveira Teixeira <stefanyoliveira.teixeira@estudante.ufjf.br>
2025-07-30 18:59:02 -0300

    Implementa função adicionarAmigo com validações de entrada e prevenção de duplicatas

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+function adicionarAmigo() {
+    const input = document.getElementById('amigo');
+    const amigo = input.value.trim();
+    
+    
+    if (amigo === '') {
+        alert('Por favor, insira um nome.');
+        return;
+    }
+    
+
+    if (amigos.includes(amigo)) {
+        alert('Este amigo já foi adicionado!');
+        return;
+    }
+    
+   
+    amigos.push(amigo);
+    
+   
+    input.value = '';
+    
+    
+    alert(`Amigo ${amigo} adicionado!`);
+    
+    
+    atualizarListaAmigos();
+}
